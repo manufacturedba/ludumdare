7a8fbf092c81df19739ca2192798640e01687248
Kaz <kaz.higaki@gmail.com>
2024-10-04 22:48:01 -0700

    kaz still getting combat working

---
+++ project/Battlefield/player_unit_spawner.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+const plSoldier = preload("res://Units/Soldier.tscn");
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_player_unit_spawn_timer_timeout() -> void:
+	var soldier: Area2D = plSoldier.instantiate().with(true);
+	soldier.global_position = global_position;
+	get_tree().current_scene.add_child(soldier);
