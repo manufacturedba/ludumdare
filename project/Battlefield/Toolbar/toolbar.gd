ad5bb9051e449cce0f7879837c068f206df7cd23
Roberto Rodriguez <manufacturedba@gmail.com>
2024-10-06 13:05:26 -0700

    Squashed commit of the following:
    commit 878e7457a6dd6710d1ef53f801030c5b66a6f77a
    Author: Roberto Rodriguez <manufacturedba@gmail.com>
    Date:   Sun Oct 6 13:05:12 2024 -0700
    
        Add portraits
    
        Increase spacing for spawn buttons a bit

--- project/Battlefield/Toolbar/toolbar.gd
+++ project/Battlefield/Toolbar/toolbar.gd
@@ -10,27 +10,27 @@ const MAX_GOLD = 99;
 
 var unitDict = {
 	CONSTANTS.CLASS_ENUM.SOLDIER: {
-		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/soldier.png",
+		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/soldier-button-portrait.png",
 		"cost": CONSTANTS.SOLDIER_COST,
 		"class": CONSTANTS.CLASS_ENUM.SOLDIER
 	},
 	CONSTANTS.CLASS_ENUM.ARCHER: {
-		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/archer.png",
+		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/archer-button-portrait.png",
 		"cost": CONSTANTS.ARCHER_COST,
 		"class": CONSTANTS.CLASS_ENUM.ARCHER
 	},
 	CONSTANTS.CLASS_ENUM.CATAPULT: {
-		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/archer.png",
+		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/archer-button-portrait.png",
 		"cost": CONSTANTS.CATAPULT_COST,
 		"class": CONSTANTS.CLASS_ENUM.CATAPULT
 	},
 	CONSTANTS.CLASS_ENUM.BRUTE: {
-		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/soldier.png",
+		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/soldier-button-portrait.png",
 		"cost": CONSTANTS.BRUTE_COST,
 		"class": CONSTANTS.CLASS_ENUM.BRUTE
 	},
 	CONSTANTS.CLASS_ENUM.SCOUT: {
-		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/soldier.png",
+		"texture": "res://Battlefield/Toolbar/UnitSpawnButton/Textures/soldier-button-portrait.png",
 		"cost": CONSTANTS.SCOUT_COST,
 		"class": CONSTANTS.CLASS_ENUM.SCOUT
 	}
@@ -45,7 +45,7 @@ var unitDict = {
 # @onready var spawner = $Spawner;
 
 var buttonWidth := 100;
-var buttonOffset := 10;
+var buttonOffset := 400;
 
 var anything := CONSTANTS.CLASS_ENUM.SOLDIER
 # Called when the node enters the scene tree for the first time.
@@ -72,9 +72,6 @@ func with(_playerUnitTypes: Array[CONSTANTS.CLASS_ENUM]):
 		var type = playerUnitTypes[index]; # put real type in
 		var typeConfig = unitDict[type];
 		var spawnButton = spawnButtonPreload.instantiate().with(self, typeConfig)
-
-		# .with(spawner, typeConfig);
-		spawnButton.position = Vector2(buttonWidth * index + buttonOffset, 0);
 		spawnButtons.push_back(spawnButton);
 		buttonContainer.add_child(spawnButton);
 	
